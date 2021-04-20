9b9e86bb2b0930e04b4e2647c832377d73e51325
asa121asa <crafta.net@gmail.com>
2021-04-21 02:51:14 +0300

    upd 2 add card

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,31 +2,39 @@ import React from 'react';
 import style from './Card.module.css'
 import union from './asses/image/Group164.svg'
 import SuperButton from "./SuperButton";
-import vector from "./asses/image/Vector.svg";
+import Tilt from 'react-tilt'
+import birds from './asses/image/birds.svg'
+import birdseEnd from './asses/image/birdseEnd.svg'
 
 
 export default function Card() {
 
-
-    /*const todolistImage = {
-        color: 'blue',
-        backgroundImage: `url(${vector})`,
-        backgroundRepeat: 'no-repeat'
-    };*/
-    return (
-        <div className={style.cardContainer}>
-            <div className={style.image}>
-                <img src={union}/>
-            </div>
-            <div className={style.titleCard}>
-                <span>Интерактивная карточка</span>
-            </div>
-            <div>
-                <SuperButton>
-                    {'Кнопка'}
-                </SuperButton>
+    return (<div className={style.test}>
+            <div className={style.coc}>
+                <img src={birds}/>
+                <img src={birdseEnd}/>
             </div>
 
+            <div className={style.bos}>
+                <Tilt className="Tilt" options={{max: 45, reverse: true, speed: 1000}}>
+
+                    <div className={style.cardContainer}>
+                        <div className={style.image}>
+                            <img src={union}/>
+                        </div>
+                        <div className={style.titleCard}>
+                            <span>Интерактивная карточка</span>
+                        </div>
+                        <div>
+                            <SuperButton>
+                                {'Кнопка'}
+                            </SuperButton>
+                        </div>
+
+
+                    </div>
+                </Tilt>
+            </div>
 
         </div>
     );
--- src/Container.tsx
+++ src/Container.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {DetailedHTMLProps, HTMLAttributes} from 'react';
 import style from './Container.module.css'
 import Fox from "./Fox";
 import Modela from "./Fox2";
 import Card from "./Card";
+import Vr from "./Vector67.png";
+import birds from './asses/image/birds.svg'
+import birdsEnd from './asses/image/birdseEnd.svg'
 
 
 function Conytainer() {
+
+    const Image = {
+        color: 'blue',
+        backgroundImage: `url(${birdsEnd})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'left',
+        backgroundSize: '120px',
+    };
+    const br = {
+        color: 'blue',
+        backgroundImage: `url(${birds})`,
+        backgroundRepeat: 'no-repeat',
+        size: '30px',
+
+    };
+
 
     return (
         <div className={style.flexContainer}>
@@ -13,10 +32,9 @@ function Conytainer() {
                 <div className={style.flexitem}>
                     <div className={style.preject}><Modela/></div>
                     <div className={style.preject}><Card/></div>
-                    <div className={style.preject}>ajlr</div>
+                    <div  className={style.preject}>ajlr</div>
                     <div className={style.preject}>ajlr</div>
                 </div>
-
             </div>
 
         </div>
